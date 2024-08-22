87b590d36768dd800d37f23e70f21de358e57e41
hanwj75 <wjws9608@gmail.com>
2024-08-22 19:20:23 +0900

    완성본(아마)
    ㅇㅇㅇ

--- game.js
+++ game.js
@@ -7,11 +7,16 @@ class Player {
     this.hp = this.maxHp; //현재 체력
     this.attackDmg = 20;
     this.heal = 30;
-    this.stageClear = 50;
+    this.run = 0;
+    this.shield = 0;
+    this.double = this.attackDmg;
   }
   //플레이어가 피격시 데미지
   userHitDmg(dmg) {
     this.hp -= dmg;
+    if (this.hp < 0) {
+      this.hp = 0; // 체력이 음수가 되지 않도록 방지
+    }
   }
   attack(monster) {
     // 플레이어의 공격
@@ -23,13 +28,30 @@ class Player {
   }
   //스테이지 클리어시 유저 체력+공격력+힐량 증가
   userLevelUp(stages) {
-    this.maxHp = 100 * stages;
-    this.hp = this.maxHp;
-    this.heal = stages * 1.5;
-    this.attackDmg += stages * 10;
+    this.hp += stages * 100;
+    this.heal = stages * 30;
+    this.attackDmg = 20 * stages;
   }
-  clearHp(next) {
-    this.hp += next;
+
+  //플레이어가 방패를 들었을때 막을 확률
+  rerollShield() {
+    const randomShield = Math.floor(Math.random() * 5) + 1;
+    this.shield = randomShield;
+    return this.shield;
+  }
+  //도망가기를 선택하였을경우 1/2확률로 도망가짐
+  runAway() {
+    const randomRun = Math.floor(Math.random() * 2) + 1;
+    this.run = randomRun;
+
+    return this.run;
+  }
+
+  doubleAttack() {
+    const randomAttack = Math.floor(Math.random() * 3) + 1;
+    this.double = randomAttack;
+
+    return;
   }
 }
 
@@ -53,7 +75,6 @@ class Monster {
       return;
     }
   }
-  //스테이지 클리어시 몬스터 체력+ 데미지+
 }
 
 function displayStatus(stage, player, monster) {
@@ -65,7 +86,7 @@ function displayStatus(stage, player, monster) {
   console.log(
     chalk.cyanBright(`|| Stage: ${stage} |`) +
       chalk.yellowBright(
-        `| Player: HP:${player.hp} DMG:${player.attackDmg} |`
+        `| Player: HP:$${player.hp} DMG:${player.attackDmg} |`
       ) +
       chalk.redBright(`| Monster: ${monster.hp} DMG:${monster.attackDmg} ||`)
   );
@@ -82,12 +103,14 @@ const battle = async (stage, player, monster) => {
   while (player.hp > 0 && monster.hp > 0) {
     console.clear();
     displayStatus(stage, player, monster);
-
+    const userShield = player.rerollShield();
+    const userRunWaye = player.runAway();
+    const doubleAtt = player.doubleAttack();
     logs.forEach((log) => console.log(log));
 
     console.log(
       chalk.green(
-        `\n1. 공격한다 2.연막탄을 던진다. 3.회복한다 4.방어한다 5.도망간다`
+        `\n1. 공격한다 2.연막탄을 던진다0~3. 3.회복한다(${player.heal}+) 4.방어한다(60%) 5.도망간다(50%) 6.연속공격(33%)`
       )
     );
     const choice = readlineSync.question("당신은");
@@ -95,14 +118,14 @@ const battle = async (stage, player, monster) => {
     // 플레이어의 선택에 따라 다음 행동 처리
     //1~5까지의 선택지를 입력할시 선택한 숫자에따라 작동함
     //선택지 외의 값을 입력할경우 경고문을 띄워줌
-    if (choice <= 5 && choice !== "") {
+    if (choice <= 6 && choice !== "") {
       logs.push(chalk.green(`${choice}를 선택하셨습니다!`));
     } else {
       logs.push(chalk.red("당신이 하지 못하는 선택입니다!"));
     }
 
     //각 번호를 선택시 할 행동 정의
-    //플레이어 행동 정의
+    //플레이어 행동 로직
     switch (choice) {
       case "1":
         logs.push(
@@ -111,10 +134,7 @@ const battle = async (stage, player, monster) => {
           )
         );
         monster.monsterHitDmg(player.attackDmg);
-        if (monster.hp < 0) {
-          stage++;
-          player.userLevelUp(stage);
-        }
+
         break;
       //플레이어가 2번의 선택지를 고르면 0~2의 랜덤한 값을가진 연막탄을 던짐
       case "2":
@@ -134,18 +154,48 @@ const battle = async (stage, player, monster) => {
         player.selfHeal(player.heal);
         break;
       case "4":
-        logs.push(chalk.yellow(`당신이 방패를 들어올립니다!`));
+        if (player.shield >= 3 && player.shield <= 5) {
+          logs.push(
+            chalk.yellow(`(${player.shield}!) 당신이 방패를 들어올립니다!`)
+          );
+        } else {
+          logs.push(chalk.yellow(`(${player.shield}!) 방패가 파괴되었습니다!`));
+        }
         break;
       case "5":
-        logs.push(chalk.yellow(`당신이 이기지못할 상대에게서 도망칩니다!`));
-        player.hp = player.maxHp;
-        monster.hp = monster.maxHp;
+        if (player.run === 1 && stage >= 1) {
+          logs.push(
+            chalk.yellow(`(${player.run}!) 당신은 꼴사납게 도망칩니다!`)
+          );
+          player.hp = stage * 100;
+          player.attackDmg = stage * 20;
+        } else {
+          logs.push(
+            chalk.yellow(`(${player.run}!) 당신은 도망에 실패했습니다..`)
+          );
+        }
+        break;
+      case "6":
+        if (player.double === 1) {
+          logs.push(chalk.yellow(`${player.double}!연속공격에 성공했습니다!`));
+          logs.push(chalk.yellow(`${player.attackDmg}의 피해를 입힙니다.`));
+          logs.push(chalk.yellow(`${player.attackDmg}의 피해를 입힙니다.`));
+          monster.monsterHitDmg(player.attackDmg * 2);
+        } else if (player.double === 2) {
+          logs.push(chalk.yellow(`${player.double}!연속공격에 성공했습니다!`));
+          logs.push(chalk.yellow(`${player.attackDmg}의 피해를 입힙니다.`));
+          logs.push(chalk.yellow(`${player.attackDmg}의 피해를 입힙니다.`));
+          logs.push(chalk.yellow(`${player.attackDmg}의 피해를 입힙니다.`));
 
+          monster.monsterHitDmg(player.attackDmg * 3);
+        } else {
+          logs.push(chalk.red(`공격에 실패했습니다!`));
+        }
         break;
     }
-    //몬스터 행동 정의
+    //몬스터 행동 로직
     if (monster.hp > 0) {
-      if (monster.stopTime >= 1) {
+      if (monster.stopTime > 0) {
         //연막탄이 유지되는동안 몬스터가 행동을 멈춤
         //턴이 지날떄마다 연막탄의 남은 턴수를 알려주고 1씩 빠짐
         logs.push(
@@ -184,15 +234,60 @@ const battle = async (stage, player, monster) => {
             player.userHitDmg(monster.attackDmg);
             break;
           case "4":
-            logs.push(chalk.red(`몬스터가 공격이 실패하자 당황합니다!`));
-
+            if (player.shield >= 3 && player.shield <= 5) {
+              logs.push(chalk.yellow(` 몬스터가 공격이 막혀 당황합니다!`));
+            } else {
+              logs.push(
+                chalk.yellow(
+                  `몬스터의 공격이 방패를 파괴합니다!${monster.attackDmg}의 피해를 받습니다!`
+                )
+              );
+              player.userHitDmg(monster.attackDmg);
+            }
             break;
           case "5":
-            logs.push(chalk.red(`몬스터가 당신을 비웃습니다...`));
+            if (player.run === 1) {
+              logs.push(chalk.red(`몬스터가 당신을 비웃습니다...`));
+              monster.hp = stage * 100;
+              monster.attackDmg = stage * 20;
+            } else {
+              logs.push(
+                chalk.red(
+                  `몬스터가 도망가는 당신에게 ${monster.attackDmg}피해를 입힙니다`
+                )
+              );
+              player.userHitDmg(monster.attackDmg);
+            }
 
+            break;
+          case "6":
+            if (player.double === 1) {
+              logs.push(chalk.red(`몬스터가 막대한 피해를 입습니다!`));
+            } else if (player.double === 2) {
+              logs.push(chalk.red(`몬스터가 절대한 피해를 입습니다!`));
+            } else {
+              logs.push(
+                chalk.red(
+                  `몬스터가 당신에게 ${monster.attackDmg * 3}의 절대한 피해를 입힙니다! `
+                )
+              );
+              player.userHitDmg(monster.attackDmg * 3);
+            }
             break;
         }
       }
+    }
+
+    if (player.hp <= 0 && monster.hp <= 0) {
+      console.clear();
+      console.log(
+        chalk.red("플레이어와 몬스터 모두 쓰러졌습니다. 당신은 죽었습니다!")
+      );
+      break;
+    }
+    if (monster.hp <= 0) {
+      stage++;
+      player.userLevelUp(stage);
     }
   }
 };
@@ -206,7 +301,23 @@ export async function startGame() {
     const monster = new Monster(stage);
     await battle(stage, player, monster);
 
-    stage++;
-    // 스테이지 클리어 및 게임 종료 조건
+    if (monster.hp <= 0) {
+      stage++;
+    }
+    //게임 클리어 혹은 종료 조건
+    if (player.hp <= 0) {
+      console.clear();
+      console.log(chalk.red("당신은 죽었습니다!"));
+      break;
+    }
+    if (stage > 10) {
+      console.clear();
+      console.log(chalk.blue("축하합니다! 모든 스테이지를 클리어 하셨습니다!"));
+      console.log();
+      console.log(chalk.blue("랭킹"));
+      displayStatus(stage - 1, player, monster);
+
+      break;
+    }
   }
 }
